0d634e7ceb54d460caa70831d322edb7026fc546
Brandon Furr <brandon.furr123@gmail.com>
2019-09-19 14:22:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,12 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-
+def batch_badge_creator(people)
+  new_array = []
+  people.each do |name|
+    new_array.push(badge_maker(name))
+  end
+  new_array
+end
     
   
